7fce934f37397ce197b86de259a1c84685cf75b6
vishnupritha <vishnuprithae@gmail.com>
2022-05-02 16:23:16 +0800

    Week1 project

--- client/index.js
+++ client/index.js
@@ -1,4 +1,8 @@
 import "./index.scss";
+const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const secp = require('@noble/secp256k1');
 
 const server = "http://localhost:3042";
 
@@ -15,15 +19,17 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   });
 });
 
-document.getElementById("transfer-amount").addEventListener('click', () => {
+document.getElementById("transfer-amount").addEventListener('click', async() => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privatekey = document.getElementById("private-key").value;
 
-  const body = JSON.stringify({
-    sender, amount, recipient
-  });
+  const messageHash = SHA256(JSON.stringify({sender, amount, recipient})).toString();
+  var signature = await secp.sign(messageHash,privatekey, 1);
+  signature = Buffer.from(signature).toString('hex');
 
+  const body = JSON.stringify({sender, amount, recipient, signature});
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const SHA256 = require('crypto-js/sha256');
+var EC = require('elliptic').ec;
+var ec = new EC('secp256k1');
+const secp = require('@noble/secp256k1');
 const port = 3042;
 
 // localhost can have cross origin errors
@@ -8,10 +12,17 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
+const privateKey1 = secp.utils.bytesToHex(secp.utils.randomPrivateKey());
+const privateKey2 = secp.utils.bytesToHex(secp.utils.randomPrivateKey());
+const privateKey3 = secp.utils.bytesToHex(secp.utils.randomPrivateKey());
+const publicKey1 = secp.utils.bytesToHex(secp.getPublicKey(privateKey1)).slice(-40);
+const publicKey2 = secp.utils.bytesToHex(secp.getPublicKey(privateKey2)).slice(-40);
+const publicKey3 = secp.utils.bytesToHex(secp.getPublicKey(privateKey3)).slice(-40);
+
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+  [publicKey1]: 100,
+  [publicKey2]: 50,
+  [publicKey3]: 75,
 }
 
 app.get('/balance/:address', (req, res) => {
@@ -21,12 +32,34 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
+  const {sender, recipient, amount, signature} = req.body;
+  console.log(req.body);
+  const msgHash = SHA256(JSON.stringify({sender, amount, recipient})).toString();
+  var publickey = secp.utils.bytesToHex(secp.recoverPublicKey(msgHash, signature, 1));
+  var publickeytemp = publickey.slice(-40);
+  console.log(publickeytemp);
+  const isValid = secp.verify(signature, msgHash, publickey);
+  console.log(isValid);
+  if(balances[publickeytemp])
+  {
   balances[sender] -= amount;
   balances[recipient] = (balances[recipient] || 0) + +amount;
   res.send({ balance: balances[sender] });
+  } else {
+    console.log("Something's wrong !! address not found on the server");
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log("Available address");
+  console.log("===================");
+  for (const i in balances){
+    console.log(`${i}(${balances[i]})`);
+  }  
+  console.log("Private Keys ");
+  console.log("===================");
+  console.log(privateKey1);
+  console.log(privateKey2);
+  console.log(privateKey3);
 });
